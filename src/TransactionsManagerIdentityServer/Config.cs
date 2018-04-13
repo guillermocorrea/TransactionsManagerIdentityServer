e4d1019faef28859f6f9f0489c8cdbbe717b03b3
Luis Guillermo Correa <guillermo.correa.99@gmail.com>
2018-04-12 23:34:47 -0500

    Added IdentityServer

---
+++ src/TransactionsManagerIdentityServer/Config.cs
@@ -0,0 +1,117 @@
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace TransactionsManagerIdentityServer
+{
+    public class Config
+    {
+        private const string API_RESOURCE_NAME = "transactions-api";
+        private IConfigurationRoot _configuration;
+
+        public Config(IConfigurationRoot configutarion)
+        {
+            _configuration = configutarion;
+        }
+
+        public IEnumerable<Client> GetClients()
+        {
+            return new List<Client> {
+                new Client
+                {
+                    ClientId = _configuration.GetSection("Clients:ApiClient:ClientId").Value,
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret(_configuration.GetSection("Clients:ApiClient:ClientSecret").Value.Sha256())
+                    },
+
+                    AllowedScopes = { API_RESOURCE_NAME }
+                },
+                new Client
+                {
+                    ClientId = _configuration.GetSection("Clients:MvcClient:ClientId").Value,
+                    ClientName = _configuration.GetSection("Clients:MvcClient:ClientName").Value,
+                    AllowedGrantTypes = GrantTypes.HybridAndClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret(_configuration.GetSection("Clients:MvcClient:ClientSecret").Value.Sha256())
+                    },
+                    RedirectUris = { _configuration.GetSection("Clients:MvcClient:RedirectUris").Value },
+                    PostLogoutRedirectUris = { _configuration.GetSection("Clients:MvcClient:PostLogoutRedirectUris").Value },
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "role",
+                        API_RESOURCE_NAME
+                    },
+                    AllowOfflineAccess = true
+                }
+            };
+        }
+
+        public IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource> {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(),
+                new IdentityResource {
+                    Name = "role",
+                    UserClaims = new List<string> {"role"}
+                }
+            };
+        }
+
+        public IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource> {
+                new ApiResource(API_RESOURCE_NAME, "Transactions API")
+            };
+        }
+
+        public List<TestUser> GetUsers()
+        {
+            return new List<TestUser> {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "assistant",
+                    Password = _configuration.GetSection("Users:Assistant:Password").Value,
+                    Claims = new List<Claim> {
+                        new Claim("name", "Assistant"),
+                        new Claim(JwtClaimTypes.Email, "assistant@email.com"),
+                        new Claim(JwtClaimTypes.Role, "assistant")
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "manager",
+                    Password = _configuration.GetSection("Users:Manager:Password").Value,
+                    Claims = new List<Claim> {
+                        new Claim("name", "Manager"),
+                        new Claim(JwtClaimTypes.Email, "manager@email.com"),
+                        new Claim(JwtClaimTypes.Role, "manager")
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "3",
+                    Username = "administrator",
+                    Password = _configuration.GetSection("Users:Administrator:Password").Value,
+                    Claims = new List<Claim> {
+                        new Claim("name", "Administrator"),
+                        new Claim(JwtClaimTypes.Email, "administrator@email.com"),
+                        new Claim(JwtClaimTypes.Role, "admin")
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ src/TransactionsManagerIdentityServer/Startup.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace TransactionsManagerIdentityServer
+{
+    public class Startup
+    {
+        public static string[] Args { get; set; } = new string[] { };
+        private ILogger logger;
+        private ILoggerFactory loggerFactory;
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(Startup.Args);
+
+            Configuration = builder.Build();
+            this.loggerFactory = loggerFactory;
+            this.loggerFactory.AddConsole(LogLevel.Information);
+            this.loggerFactory.AddDebug();
+            this.logger = this.loggerFactory.CreateLogger("Startup");
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            Config config = new Config(Configuration);
+            services.AddIdentityServer()
+                .AddInMemoryClients(config.GetClients())
+                .AddInMemoryIdentityResources(config.GetIdentityResources())
+                .AddInMemoryApiResources(config.GetApiResources())
+                .AddTestUsers(config.GetUsers())
+                .AddDeveloperSigningCredential();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
